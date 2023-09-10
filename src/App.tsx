95fd24c19584922e0154e79f3a0496ad30b4946e
Mandy-cyber <acm.rodriques@gmail.com>
2023-09-10 01:44:40 -0400

    adding help command + fixing other commands

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,43 @@ import React, { useState } from 'react';
 import { ReactTerminal } from "react-terminal";
 import {colors} from './style/colors.ts';
 import { HomePageBody } from './App.styles.ts';
-import { welcomeMessage, listPages } from './commands.tsx';
-import { PAGES } from "./constants.ts"
+import { PAGES } from "./constants.ts";
+import { 
+  welcomeMessage, 
+  listPages, 
+  helpMessage 
+} from './commands.tsx';
 
 function App() {
-  const [pwd, setPwd] = useState("~");
+  const ROOT_PAGE: string = "~";
+  const [pwd, setPwd] = useState(ROOT_PAGE);
 
 
   const commands = {
+    // GENERAL
     whoami: "mandy-cyber",
-    ls: listPages,
     pwd: pwd,
+    help: helpMessage,
 
-    cd: (directory: string) => {
-      if (directory in PAGES) {
-        setPwd(pwd + "/" + directory)
+    // LIST FILES
+    ls: () => {
+      if (pwd === ROOT_PAGE) {
+        return listPages;
       } else {
-        return <p>bash: cd: {directory}: No such file or directory</p>
+        return <p>Nothing to see here...</p>
+      }
+    },
+
+    // CHANGE DIRECTORY
+    cd: (directory: string) => {
+      if (directory === "..") {
+        setPwd(ROOT_PAGE)
+      } else {
+        if (PAGES.includes(directory) && pwd === ROOT_PAGE) {
+          setPwd(pwd + "/" + directory)
+        } else {
+          return <p>bash: cd: {directory}: No such file or directory</p>
+        }
       }
     }
   };
@@ -26,20 +46,20 @@ function App() {
   return (
     <>
     <HomePageBody>
-    
-      <ReactTerminal
-          welcomeMessage={welcomeMessage}
-          prompt={"stranger@mandy-world:" + pwd + "$"}
-          commands={commands}
-          themes={{
-            "custom-theme": {
-              themeBGColor: colors.DARK_GRAY,
-              themeColor: colors.LIGHT_PURPLE,
-              themePromptColor: colors.PINK
-            }
-          }}
-          theme="custom-theme" 
-      />
+  
+        <ReactTerminal
+            welcomeMessage={welcomeMessage}
+            prompt={"stranger@mandy-world:" + pwd + " $"}
+            commands={commands}
+            themes={{
+              "custom-theme": {
+                themeBGColor: colors.DARK_GRAY,
+                themeColor: colors.LIGHT_PURPLE,
+                themePromptColor: colors.PINK
+              }
+            }}
+            theme="custom-theme"
+        />
 
     </HomePageBody>
   </>
--- src/commands.tsx
+++ src/commands.tsx
@@ -1,15 +1,15 @@
 import React from "react";
-import { PAGES } from "./constants.ts"
+import { PAGES } from "./constants.ts";
 
 
 const welcomeMessage = (
     <div>
-        <p>Hey there! Type <code>help</code> to get started</p>
+        <p>Hey there! Type <b><code>help</code></b> to get started</p>
     </div>
 );
 
 
-const pages = PAGES.map(page => <p>{page}</p>);
+const pages = PAGES.map(page => <p>-- {page}</p>);
 const listPages = (
     <div>
         {pages}
@@ -17,4 +17,34 @@ const listPages = (
 );
 
 
-export { welcomeMessage, listPages };
+const helpMessage = (
+    <pre>
+        <b>help: help</b><br />
+        Display information about builtin commands
+
+        <br/><br/>
+        <b>Commands:</b>
+        <br/>All the commands you can run in this terminal<br/>
+        <br/> __ <code>cd directory</code>
+        <br/> |___ change to the given directory
+        <br/> 
+        <br/> __ <code>ls</code>
+        <br/> |___ list visible files and directories
+        <br/>
+        <br/> __ <code>pwd</code>
+        <br/> |___ echo the present working directory
+        <br/>
+        <br/> __ <code>whoami</code>
+        <br/> |___ see who you are...
+        <br/>
+        <br/> __ <code>clear</code>
+        <br/> |___ clears the terminal
+        <br/>
+        <br/> __ <code>help</code>
+        <br/> |___ re-display this information
+
+    </pre>
+);
+
+
+export { welcomeMessage, listPages, helpMessage };
