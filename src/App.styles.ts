3874a12160f0bc57a10fdabdce75101f23664fba
Mandy-cyber <acm.rodriques@gmail.com>
2023-09-10 01:43:47 -0400

    making whole page the same bgcolor

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import {colors} from './style/colors.ts';
 
 const HomePageBody = styled.div`
+    height: 100vh;
     background-color: ${colors.DARK_GRAY};
 `;
 
