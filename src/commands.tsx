5c2bb966649ff9f732051d6ffe78fc947e916ff7
Mandy-cyber <acm.rodriques@gmail.com>
2023-09-10 02:19:07 -0400

    added resume command to open in gdrive

--- src/commands.tsx
+++ src/commands.tsx
@@ -31,15 +31,27 @@ const helpMessage = (
         <br/> __ <code>ls</code>
         <br/> |___ list visible files and directories
         <br/>
+        <br/> __ <code>clear</code>
+        <br/> |___ clears the terminal
+        <br/>
         <br/> __ <code>pwd</code>
         <br/> |___ echo the present working directory
         <br/>
         <br/> __ <code>whoami</code>
         <br/> |___ see who you are...
         <br/>
-        <br/> __ <code>clear</code>
-        <br/> |___ clears the terminal
+        <br/> __ <code>joke</code>
+        <br/> |___ tell an (admittedly awful) joke
         <br/>
+        <br/> __ <code>resume</code>
+        <br/> |___ opens the resume of Mandy-cyber
+        <br/>
+        <br/> __ <code>view [-github, -linkedin]</code>
+        <br/> |___ goes to GitHub or LinkedIn profile
+
+
+
+        <br/><br/>
         <br/> __ <code>help</code>
         <br/> |___ re-display this information
 
