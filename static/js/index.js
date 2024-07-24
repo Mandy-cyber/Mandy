0c968140cd6220e4a845d546da15f02aeac4b182
Mandy-cyber (Laptop) <acm.rodriques@gmail.com>
2024-07-23 22:03:28 -0500

    adding floating icons & icon animations

--- static/js/index.js
+++ static/js/index.js
@@ -86,12 +86,37 @@ function showSlides(n) {
 // stop autoscroll when arrows are used
 var nextBtn = document.getElementById("next")
 var prevBtn = document.getElementById("prev")
-
 document.addEventListener('DOMContentLoaded', function() {
     nextBtn.addEventListener('click', function() {
         clearInterval(slideInterval)
     });
     prevBtn.addEventListener('click', function() {
         clearInterval(slideInterval)
+    });
+});
+
+
+// show hidden icons + change icon animations
+var waveIcon = document.getElementById("wave-icon")
+var hiddenIcons = document.getElementsByClassName("hidden-floating")
+
+document.addEventListener('DOMContentLoaded', function() {
+    waveIcon.addEventListener('click', function() {
+        if (waveIcon.classList.contains('animate__pulse')) {
+            waveIcon.classList.remove('animate__pulse')
+        } else {
+            waveIcon.classList.add('animate__pulse')
+        }
+
+        for (let icon of hiddenIcons) {
+            if (icon.classList.contains('animate__slideInRight')) {
+                icon.classList.remove('animate__slideInRight');
+                icon.classList.add('animate__slideOutRight');
+            } else {
+                icon.style.display = "flex";
+                icon.classList.remove('animate__slideOutRight');
+                icon.classList.add('animate__slideInRight');
+            }
+        }
     });
 });
